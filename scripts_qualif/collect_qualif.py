1cf45a21fb6127dce0936efdb438419eb643a909
Anthony Assaf <60676144+anthonybassaf@users.noreply.github.com>
2023-07-24 18:12:02 +0200

    Update collect_qualif.py

--- scripts_qualif/collect_qualif.py
+++ scripts_qualif/collect_qualif.py
@@ -1,6 +1,6 @@
 import requests
 import time
-import datetime
+from datetime import datetime, timedelta
 import csv
 import sys
 import yaml
@@ -84,27 +84,27 @@ class CSVDataWriter(object):
         metric_list = []
 
         # Create Metric based on different tables in the YAML file
-        if "CPU" in self.table_name:
-            # print(f"Creating new metric with query: {query}\n")
-            metric_list.append(Metric(name, site, mode, query, availability_zone, aggregates, memory, product_name))
-            print("Creating New Metric: ", metric_list[-1].__dict__)
-        elif "DMI_INFO" in self.table_name:
-            # print(f"Creating new metric with query: {query}\n")
-            metric_list.append(Metric(name, site, mode, query, availability_zone, aggregates, memory, product_name))
-            print("Creating New Metric: ", metric_list[-1].__dict__)
-        elif "POWER" in self.table_name:
-            # print(f"Creating new metric with query: {query}\n")
-            metric_list.append(Metric(name, site, mode, query, availability_zone, aggregates, memory, product_name))
-            print("Creating New Metric: ", metric_list[-1].__dict__)
-        elif "RAM" in self.table_name:
-            # print(f"Creating new metric with query: {query}\n")
-            metric_list.append(Metric(name, site, mode, query, availability_zone, aggregates, memory, product_name))
-            print("Creating New Metric: ", metric_list[-1].__dict__)
-        elif "VMS" in self.table_name:
-            # print(f"Creating new metric with query: {query}\n")
-            metric_list.append(Metric(name, site, mode, query, availability_zone, aggregates, memory, product_name))
-            print("Creating New Metric: ", metric_list[-1].__dict__)
-        elif "TRIGRAMME" in self.table_name:
+        # if "CPU" in self.table_name:
+        #     # print(f"Creating new metric with query: {query}\n")
+        #     metric_list.append(Metric(name, site, mode, query, availability_zone, aggregates, memory, product_name))
+        #     print("Creating New Metric: ", metric_list[-1].__dict__)
+        # elif "DMI_INFO" in self.table_name:
+        #     # print(f"Creating new metric with query: {query}\n")
+        #     metric_list.append(Metric(name, site, mode, query, availability_zone, aggregates, memory, product_name))
+        #     print("Creating New Metric: ", metric_list[-1].__dict__)
+        # elif "POWER" in self.table_name:
+        #     # print(f"Creating new metric with query: {query}\n")
+        #     metric_list.append(Metric(name, site, mode, query, availability_zone, aggregates, memory, product_name))
+        #     print("Creating New Metric: ", metric_list[-1].__dict__)
+        # elif "RAM" in self.table_name:
+        #     # print(f"Creating new metric with query: {query}\n")
+        #     metric_list.append(Metric(name, site, mode, query, availability_zone, aggregates, memory, product_name))
+        #     print("Creating New Metric: ", metric_list[-1].__dict__)
+        # elif "VMS" in self.table_name:
+        #     # print(f"Creating new metric with query: {query}\n")
+        #     metric_list.append(Metric(name, site, mode, query, availability_zone, aggregates, memory, product_name))
+        #     print("Creating New Metric: ", metric_list[-1].__dict__)
+        if "TRIGRAMME" in self.table_name:
             # print(f"Creating new metric with query: {query}\n")
             metric_list.append(MetricTrigramme(name, site, mode, query, availability_zone, aggregates, memory, product_name, metric_from_file['trigramme']))
             print("Creating New Metric: ", metric_list[-1].__dict__)
@@ -121,8 +121,9 @@ class CSVDataWriter(object):
         return metric_list
 
     def processMetric(self, table_name, metric_read, metric_from_file, my_date):
-        print("\nTABLE: %s -- METRIC: %s -- Add metric to the CSV file" % (table_name, metric_read))
+        print("\nTABLE: %s -- METRIC: %s -- Add metric to the CSV file %s_%s.csv" % (table_name, metric_read, table_name, my_date))
         metric_list = self.createNewMetrics(metric_from_file, my_date)
+        print(f"\nCreating New Metric: ", metric_list[-1].__dict__)
         # print("Metric List: ", [metric.__dict__ for metric in metric_list], "\n")  # This prints all the attributes of the metrics in the list
         for metric in metric_list:
             # if isinstance(metric, (MetricTrigramme, MetricServer, MetricGlobale)):
@@ -153,13 +154,25 @@ def list_tables():
         raise
 
 def parse_args():
-    parser = argparse.ArgumentParser(usage="%(prog)s --metricdate YYYYMMDD --tablename TABLE [optional] --metricname METRIC [optional] \n Use --list for an overview of Tables and metrics \n")
-    parser.add_argument("metricdate", help="The date of the day you want to collect")
+    parser = argparse.ArgumentParser(usage="%(prog)s --startdate YYYYMMDD --enddate YYYYMMDD --tablename TABLE [optional] --metricname METRIC [optional] \n Use --list for an overview of Tables and metrics \n")
+    parser.add_argument("--startdate", help="The start date of the period you want to collect")
+    parser.add_argument("--enddate", help="The end date of the period you want to collect")
     parser.add_argument("--tablename", help="Specify a table name if you want only a file for this table, or leave it blank to get a list of available tables")
     parser.add_argument("--metricname", help="Specify a metric name if you want only a file for this metric (table name required)")
     parser.add_argument("--list", action='store_true', help="Lists all the tables and metrics available to collect")
     args = parser.parse_args()
-    if args.metricdate is None:
+    
+    # check if only one date argument is provided
+    if args.startdate and not args.enddate:
+        args.enddate = args.startdate
+    if args.enddate and not args.startdate:
+        args.startdate = args.enddate
+        
+    # check if start date is later than end date
+    if datetime.strptime(args.startdate, "%Y%m%d") > datetime.strptime(args.enddate, "%Y%m%d"):
+        raise ValueError("Start date cannot be later than end date.")
+    
+    if args.startdate is None or args.enddate is None:
         print("Specify the date of the day you want to collect. Format YYYYMMDD \n")
         parser.print_usage()
         sys.exit(1)
@@ -172,45 +185,46 @@ def parse_args():
         sys.exit(0)  # Exit after printing the tables
     return args
 
-
-
 if __name__ == "__main__":
-    os.makedirs(f"{cfg.app_directory}/tmp", exist_ok=True)
+    os.makedirs(f"{cfg.app_directory}/csv_files", exist_ok=True)
     args = parse_args()
-    my_date = args.metricdate
+    start_date = datetime.strptime(args.startdate, "%Y%m%d")
+    end_date = datetime.strptime(args.enddate, "%Y%m%d")
 
-    myfrom = int(time.mktime(datetime.datetime.strptime("%s 00:00:00" % my_date, "%Y%m%d %H:%M:%S").timetuple()))
-    myuntil = int(time.mktime(datetime.datetime.strptime("%s 23:59:59" % my_date, "%Y%m%d %H:%M:%S").timetuple()))
+    while start_date <= end_date:
+        my_date = start_date.strftime("%Y%m%d")
+        myfrom = int(time.mktime(datetime.strptime("%s 00:00:00" % my_date, "%Y%m%d %H:%M:%S").timetuple()))
+        myuntil = int(time.mktime(datetime.strptime("%s 23:59:59" % my_date, "%Y%m%d %H:%M:%S").timetuple()))
 
-    try:
-        os.makedirs(cfg.file_dest_dir, exist_ok=True)
-        global_writer = CSVGlobaWriter("%s/%s_%s.txt" % (cfg.file_dest_dir.replace("\\", "/"), cfg.prefixe_GlobalFilename, my_date))
+        try:
+            os.makedirs(cfg.file_dest_dir, exist_ok=True)
+            global_writer = CSVGlobaWriter("%s/%s_%s.txt" % (cfg.file_dest_dir.replace("\\", "/"), cfg.prefixe_GlobalFilename, my_date))
 
+            with open(cfg.file_path_metrics) as metrics_file:
+                metrics_yml = yaml.safe_load(metrics_file)
 
-        with open(cfg.file_path_metrics) as metrics_file:
-            metrics_yml = yaml.safe_load(metrics_file)
+                if args.tablename:
+                    table_name = args.tablename
+                    csvDwriter = CSVDataWriter(f"{cfg.app_directory}/csv_files/", table_name)
 
-            if args.tablename:
-                table_name = args.tablename
-                csvDwriter = CSVDataWriter(f"{cfg.app_directory}/tmp/{table_name}_{my_date}.csv", table_name)
-
-                if args.metricname:
-                    csvDwriter.processMetric(table_name, args.metricname, metrics_yml['TABLES'][table_name][args.metricname], my_date)
-                else:
-                    for metric_read in metrics_yml['TABLES'][table_name]:
-                        csvDwriter.processMetric(table_name, metric_read, metrics_yml['TABLES'][table_name][metric_read], my_date)
-                csvDwriter.file.close()
-                global_writer.addFileRow(csvDwriter.filename)
-            else:
-                for table_name in metrics_yml['TABLES']:
-                    csvDwriter = CSVDataWriter("%s/tmp/%s_%s.csv" % (cfg.app_directory, table_name, my_date), table_name)
-                    for metric_read in metrics_yml['TABLES'][table_name]:
-                        csvDwriter.processMetric(table_name, metric_read, metrics_yml['TABLES'][table_name][metric_read], my_date)
+                    if args.metricname:
+                        csvDwriter.processMetric(table_name, args.metricname, metrics_yml['TABLES'][table_name][args.metricname], my_date)
+                    else:
+                        for metric_read in metrics_yml['TABLES'][table_name]:
+                            csvDwriter.processMetric(table_name, metric_read, metrics_yml['TABLES'][table_name][metric_read], my_date)
                     csvDwriter.file.close()
                     global_writer.addFileRow(csvDwriter.filename)
+                else:
+                    for table_name in metrics_yml['TABLES']:
+                        csvDwriter = CSVDataWriter("%s/csv_files/%s_%s.csv" % (cfg.app_directory, table_name, my_date), table_name)
+                        for metric_read in metrics_yml['TABLES'][table_name]:
+                            csvDwriter.processMetric(table_name, metric_read, metrics_yml['TABLES'][table_name][metric_read], my_date)
+                        csvDwriter.file.close()
+                        global_writer.addFileRow(csvDwriter.filename)
 
-            global_writer.file.close()
-    except IOError:
-        print("Error while reading the metrics file. Does it exist?")
-        raise
+                global_writer.file.close()
+        except IOError:
+            print("Error while reading the metrics file. Does it exist?")
+            raise
 
+        start_date += timedelta(days=1)  # advance to next day
